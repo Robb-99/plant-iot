464b38e9ea903a2606cf32a987a2e3049dcf981e
Ro_B <robin.baeuerlein@gmail.com>
2022-07-29 18:53:15 +0200

    [feat] include drawPIL call for demo purposes

--- sketch_jul22a.ino
+++ sketch_jul22a.ino
@@ -1,5 +1,7 @@
 #include <FastLED.h>
 
+#define IMDVerse
+
 #define PIN_SOUND 12
 int freq[4] = {440, 580, 650, 720};
 
@@ -13,7 +15,9 @@ int soilMoistureValue = 0;
 int soilmoisturepercent = 0;
 int ledPercentValue = 0;
 
-const int soilThreeshold = 25;
+const int soilUrgent = 10;
+const int soilDry = 20;
+const int soilTooWet = 70;
 
 /*
  * Reprents led indices to draw the pil pattern
@@ -55,9 +59,9 @@ const int pilLEDPattern[28] =
  */
 void drawPIL()
 {
-  for(int i = 0; i < sizeof(pilLEDPattern) / sizeof(pilLEDPattern[0]); i++) leds[pilLEDPattern[i]] = CRGB(0, 255, 0);
-  
+   for(int i = 0; i < sizeof(pilLEDPattern) / sizeof(pilLEDPattern[0]); i++) leds[pilLEDPattern[i]] = CRGB(0, 255, 0);
 }
+
 
 void alertSound(){
    playTone(freq[0], 150, 30);
@@ -84,28 +88,54 @@ void setup() {
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   ledPercentValue = 100 / NUM_LEDS;
-  Serial.println(ledPercentValue);
+  //Serial.println(ledPercentValue);
 }
 
 void loop() {
   soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
   soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-  Serial.print(soilmoisturepercent);
-  Serial.println("%");
+  //Serial.print(soilmoisturepercent);
+  //Serial.println("%");
   FastLED.clear(true);
   int ledsPercentActive = 0;
-  for (int i = 0; i <= NUM_LEDS; i++) {
-    ledsPercentActive += ledPercentValue;
-    leds[i] = CRGB(soilmoisturepercent >= soilThreeshold ? 0 : 255, soilmoisturepercent >= soilThreeshold ? 255 : 0, 0);
-    if(ledsPercentActive > soilmoisturepercent){
-      break;
+  //Zustände: Rot soilThreeshold <35, Grün  35-52, Blau >52
+  if (soilmoisturepercent <= soilDry){
+    for (int i = 0; i <= NUM_LEDS; i++) {
+      ledsPercentActive += ledPercentValue;
+      leds[i] = CRGB(255,  0,   0); 
+      if(ledsPercentActive > soilmoisturepercent){
+        break;
+      }
     }
   }
-  Serial.println();
+  #ifdef IMDVerse
+  else if (soilmoisturepercent > soilDry && soilmoisturepercent <= soilTooWet) drawPIL();
+  
+  #else
+  else if (soilmoisturepercent > soilDry && soilmoisturepercent <= soilTooWet){
+    for (int i = 0; i <= NUM_LEDS; i++) {
+      ledsPercentActive += ledPercentValue;
+      leds[i] = CRGB(0,  255,   0); 
+      if(ledsPercentActive > soilmoisturepercent){
+        break;
+      }
+    }
+  }
+  #endif
+  else if (soilmoisturepercent > soilTooWet){
+    for (int i = 0; i <= NUM_LEDS; i++) {
+      ledsPercentActive += ledPercentValue;
+      leds[i] = CRGB(0,  0,   255); 
+      if(ledsPercentActive > soilmoisturepercent){
+        break;
+      }
+    }
+  }
+  //SSerial.println();
   FastLED.show();
-  if(soilmoisturepercent < soilThreeshold){
-    Serial.println("Sound");
+  if(soilmoisturepercent < soilUrgent){
+ //Serial.println("Sound");
     alertSound();
   }
-  delay(250);
+  delay(20000);
 }
